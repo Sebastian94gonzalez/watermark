78cd7b540dd7d4b39f05978a004ab23398436557
Sebastian Gonzalez <sebas94gonzalez@gmail.com>
2023-03-14 16:42:07 -0500

    Path for Mnau (03/14/23)

--- watermar_layer.py
+++ watermar_layer.py
@@ -3,8 +3,8 @@ import os
 
 # https://pillow.readthedocs.io/en/stable/reference/Image.html
 
-# dir_path = r'C:\Users\se_ba\Desktop\watermark\sample_images'
-dir_path = r'D:\MADRIGUERA\01 MADRIGUERA STUDIO\01 FIVERR\WATERMARK'
+dir_path = r'C:\Users\se_ba\Desktop\watermark\sample_images'
+        dir_path = r'D:\MADRIGUERA\01 MADRIGUERA STUDIO\01 FIVERR\WATERMARK'
 
 # Iterate directory 
 for image in os.listdir(dir_path):
@@ -16,8 +16,8 @@ for image in os.listdir(dir_path):
         width, height = img1.size
         print('IMAGE SIZE : ' + str(width) + ', ' + str(height))
         # Opening the secondary image (overlay image)
-        # img2 = Image.open(r'C:\Users\se_ba\Desktop\watermark\Fiverr Watermark 4000x4000.png')
-        img2 = Image.open(r'D:\MADRIGUERA\01 MADRIGUERA STUDIO\01 FIVERR\WATERMARK\Resources\Fiverr Watermark 4000x4000.png')
+        img2 = Image.open(r'C:\Users\se_ba\Desktop\watermark\Fiverr Watermark 4000x4000.png')
+        # img2 = Image.open(r'D:\MADRIGUERA\01 MADRIGUERA STUDIO\01 FIVERR\WATERMARK\Resources\Fiverr Watermark 4000x4000.png')
         
         print('Image 2 size: ' + str(img2.size))
         
@@ -33,5 +33,5 @@ for image in os.listdir(dir_path):
         # img1.show()
         list = image.split('.')
         # print(list)
-        # img1.save(dir_path + '\\test\\' + image) 
-        img1.save(dir_path + image)
+        img1.save(dir_path + '\\test\\' + image) 
+        # img1.save(dir_path + image)
