06a84a34989909207f217a0c981f3a5815b35e84
Sebastian Gonzalez <sebas94gonzalez@gmail.com>
2024-07-05 15:39:39 -0500

    Changed output to create folder within directory.

--- TempExeConstrct/watermark_layer.py
+++ TempExeConstrct/watermark_layer.py
@@ -9,52 +9,47 @@ from tkinter import filedialog
 # https://pillow.readthedocs.io/en/stable/reference/Image.html
 
 def openFolder():
-    dir_path = filedialog.askdirectory(title='+++++++++ SELECT FOLDER WITH IMAGES +++++++++')
-    start(dir_path)
+    dir_path = filedialog.askdirectory(title='+++++++++ SELECT FOLDER CONTAINING THE IMAGES +++++++++')
+    # Create an output folder where all non directory files will land
+    if 'WaterMarkedOutput' not in os.listdir(dir_path):
+        os.mkdir(os.path.join(dir_path, 'WaterMarkedOutput')) 
+    # Location where all items will be moved to
+    target_dir = dir_path + '\WaterMarkedOutput'
+    start(dir_path, target_dir)
     
-def start(dir_path):
+def start(dir_path, target_dir):
     # Loop through directory 
     for image in os.listdir(dir_path):
         # print('dir_pth : ' + dir_path)
         print('Image Name : ' + image)
-        if '.png' in image and image != 'MadrigueraWatermark_4000x4000.png':
+        if ('.png' or '.jpg' in image) and os.path.isfile(dir_path + '\\' + image):
             
             # Opening the primary image (used in background)
             img1 = PILImage.open(dir_path + '\\' + image)
             width, height = img1.size
             print('IMAGE SIZE : ' + str(width) + ', ' + str(height))
             
-            # Opening the secondary image (overlay image)
+            # Opening the secondary image (overlay watermark image)
             if getattr(sys, 'frozen', False):
                 # Running in a bundle
                 bundle_dir = sys._MEIPASS
             else:
-                # Running in a normal Python environment
+                # Running in a normal Python IDE
                 bundle_dir = os.path.dirname(os.path.abspath(__file__))
 
             img2_path = os.path.join(bundle_dir, 'MadrigueraWatermark_4000x4000.png')
             img2 = PILImage.open(img2_path)
-            
-            print('Image 2 size: ' + str(img2.size))
-            print('AFTER Image 2 size: ' + str(img2.size))
 
             # Pasting img2 image on top of img1 
             img1.paste(img2, box=None, mask = img2)
             
-            # Split 'png' from the file to later insert '_ForRevision' in between
-            pngSplit = image.split('.')
-
-            # print(pngSplit)
-            # Save Image
-            imageNameNoExtension = image[0:len(image) - len(pngSplit[1]) - 1]
-            print(imageNameNoExtension)
-            img1.save(dir_path + '\\' + imageNameNoExtension + '_ForRevision.' + pngSplit[1]) 
-            # img1.save(dir_path + '\\' + pngSplit[0] + '_ForRevision.' + pngSplit[1]) 
+            # Save image on to the WaterMarkedOutput folder
+            img1.save(target_dir + '\\' + image) 
             
 # Set up GUI
 window = Tk()
 Label(window
-      , text='IMPORTANT NOTES:\n - All image must be within the same folder\n - All images to be watermarked must be of \'.jpg\' format'
+      , text='IMPORTANT NOTES:\n - All image must be within the same folder\n - All images to be watermarked must be of \'.jpg\' OR \'.png\'format'
       , font=('Aerial 14'),).pack()
 
 # Get screen width and height
@@ -62,7 +57,7 @@ screen_width = window.winfo_screenwidth()
 screen_height = window.winfo_screenheight()
 
 # Set window size
-window_width = 500
+window_width = 600
 window_height = 150
 
 # Calculate x and y offsets to center the window
